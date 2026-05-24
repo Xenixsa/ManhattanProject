ec16460f8efc6bd3cf20485168eea68a8809b455
Xenixsa <98410025+Xenixsa@users.noreply.github.com>
2026-05-24 23:44:21 +0200

    Setting framework.

--- app/src/main/java/com/settings/SettingsManager.java
+++ app/src/main/java/com/settings/SettingsManager.java
@@ -1,4 +1,56 @@
 package com.settings;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class SettingsManager {
+
+    private static final String File = "settings.properties";
+    private Properties properties = new Properties();
+
+    public SettingsManager(){
+        load();
+    }
+
+    private void load(){
+        try {
+            File file = new File(File);
+            if (!file.exists()){
+                save();
+                return;
+            }
+            FileInputStream fileInputStream = new FileInputStream(File);
+            properties.load(fileInputStream);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void save(){
+        try {
+            FileOutputStream outputStream = new FileOutputStream(File);
+            properties.store(outputStream,"SimulationSettings");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getStringSetting(String key,String defaultStringValue){
+        return properties.getProperty(key,defaultStringValue);
+    }
+
+    public int getIntSetting(String key,int defaultIntValue){
+        return Integer.parseInt(properties.getProperty(key,String.valueOf(defaultIntValue)));
+    }
+
+    public void set(String key,int value){
+        properties.setProperty(key,String.valueOf(value));
+    }
+    public void set(String key, String value){
+        properties.setProperty(key, value);
+    }
+
 }
